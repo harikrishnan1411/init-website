e90a3519f1841e25163775826aa7530753a7fe3e
T S Harikrishnan <125595498+harikrishnan1411@users.noreply.github.com>
2024-09-19 21:46:10 +0530

    fixed color theme issue

--- myapp/tailwind.config.js
+++ myapp/tailwind.config.js
@@ -1,11 +1,15 @@
-// tailwind.config.js
 module.exports = {
+  darkMode: 'class', // Enables dark mode via 'class' strategy
   content: [
-    './views/**/*.hbs', // Adjust this line if you have a different file structure
-    './public/**/*.html',
+    './views/**/*.hbs', // Include all Handlebars files in the views directory
+    './public/**/*.html', // Include all HTML files in the public directory
   ],
   theme: {
-    extend: {},
+    extend: {
+      // You can add custom theme settings here if needed
+    },
   },
-  plugins: [],
+  plugins: [
+    // Add any Tailwind plugins you might need here
+  ],
 }
